d89e10593d2a3524c53e994fff2f3c71f17366f8
PRASHNAT SINGH TOMAR <108085707+SINGH1263@users.noreply.github.com>
2024-09-15 15:14:03 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SINGH1263",
   resume:
-    "https://drive.google.com/file/d/1XApQMu-6YwtnphBKlT52BkVjtp8F4s0D/view?usp=drive_link",
+    "https://drive.google.com/file/d/19rANyU8i9IzrSazEJDjjJmIFftQXmBcz/view?usp=sharing",
   linkedin: "www.linkedin.com/in/prashant-singh-tomar-a0ba65225",
   twitter:"https://x.com/PRA_SHAANT_7781",
   insta: "https://www.instagram.com/prashant_7781/",
